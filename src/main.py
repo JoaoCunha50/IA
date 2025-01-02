8475917b62a1b02c6a220c03cfcc7e8fcd2fa64f
João Sá <joaopedrorsa@gmail.com>
2025-01-02 10:24:53 +0000

    :construction: feat: Added the Hill Climbing and Greedy algorithm

--- src/main.py
+++ src/main.py
@@ -65,7 +65,7 @@ def main():
         print(Fore.CYAN + "3-" + Fore.WHITE + " Imprimir arestas de Grafo")
         print(Fore.CYAN + "4-" + Fore.WHITE + " Imprimir veículos disponíveis")
         print(Fore.CYAN + "5-" + Fore.WHITE + " Realizar busca cega (DFS/BFS/UCS)")
-        print(Fore.CYAN + "6-" + Fore.WHITE + " Realizar busca informada (A*/Greedy)")
+        print(Fore.CYAN + "6-" + Fore.WHITE + " Realizar busca informada (A*/Greedy/Hill Climbing)")
         print(Fore.CYAN + "0-" + Fore.WHITE + " Sair")
         print()
 
@@ -123,23 +123,29 @@ def main():
         elif saida == 6:
             print("\nEscolha o algoritmo de busca informada:")
             print(Fore.CYAN + "1-" + Fore.WHITE + " A*")
+            print(Fore.CYAN + "2-" + Fore.WHITE + " Greedy")
+            print(Fore.CYAN + "3-" + Fore.WHITE + " Hill Climbing")
             algoritmo = int(input(Fore.MAGENTA + "Introduza a sua opção -> " + Fore.WHITE))
 
             if algoritmo == 1:
                 resultados = g.aStar_multiple_dest("pedome", ordered_names)
                 nome_algoritmo = "A*"
+            elif algoritmo == 2:
+                resultados = g.greedy_multiple_dest("pedome", ordered_names)
+                nome_algoritmo = "Greedy"
+            elif algoritmo == 3:
+                resultados = g.hillClimbing_multiple_dest("pedome", ordered_names)
+                nome_algoritmo = "Hill Climbing"
             else:
                 print(Fore.RED + "Opção inválida!")
                 continue
             
-            print(f"\nPROCURA {nome_algoritmo}")
+            print(Fore.CYAN + f"\nPROCURA {nome_algoritmo}")
             for destino, (caminho,custo,visitados) in resultados.items():
                if caminho is None:
-                    print()
                     print(Fore.RED + "Não foi possível encontrar um caminho para " + Fore.WHITE + f"{destino}")
                     print()
                else:
-                    print()
                     print(Fore.GREEN + "Para destino " + Fore.WHITE + f"{destino}:")
                     print(Fore.GREEN + "Caminho: " + Fore.WHITE + f"{caminho}")
                     print(Fore.GREEN + "Custo: " + Fore.WHITE + f"{custo}")
